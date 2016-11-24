a0410334f1c13730bd18485a681daa6dc5919be2
shaojung <shaojung@gmail.com>
2016-11-24 21:23:45 +0800

    set activity to LocationListener

--- app/src/main/java/com/example/yvtc/yvn112401/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvn112401/MainActivity.java
@@ -12,7 +12,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LocationListener {
     // 定位管理器
     private LocationManager mLocationManager;
     // 定位監聽器
@@ -26,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
         mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.removeUpdates(this);
+    }
+
     public void click1(View v) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -37,27 +54,27 @@ public class MainActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
-                new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        Log.d("LOC", "Change!!");
-                    }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
 
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("LOC", "Change!!");
+        Log.d("LOC", "" + location.getLatitude() + "," + location.getLongitude());
+    }
 
-                    }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-                    @Override
-                    public void onProviderEnabled(String provider) {
+    }
 
-                    }
+    @Override
+    public void onProviderEnabled(String provider) {
 
-                    @Override
-                    public void onProviderDisabled(String provider) {
+    }
 
-                    }
-                });
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
